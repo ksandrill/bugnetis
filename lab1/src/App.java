4d850797d05721605fef58c8dddd77d87c11a073
ksandrill <edgyestlord@gmail.com>
2020-09-10 00:48:54 +0700

    done

--- lab1/src/App.java
+++ lab1/src/App.java
@@ -4,7 +4,7 @@ import java.net.SocketAddress;
 public class App {
     public static void main(String[] args) throws IOException {
 
-       UdpCloneDetector detector = new UdpCloneDetector("224.0.147.0",8081,3000,3000);
+       UdpCloneDetector detector = new UdpCloneDetector("224.0.147.0",8081,2000,3000);
        detector.run();
 
     }
--- lab1/src/UdpCloneDetector.java
+++ lab1/src/UdpCloneDetector.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Map;
 
 public class UdpCloneDetector {
-    private  MulticastSocket socket;
+    private MulticastSocket socket;
     private final SocketAddress multicastAddr;
     private final int TIMEOUT;
     private final int UPDATE_TIMEOUT;
@@ -14,34 +14,34 @@ public class UdpCloneDetector {
     private long lastRecvtime;
 
     public UdpCloneDetector(String multicastAddr, int port, int timeout, int updateTimeout) throws IOException {
-        this.multicastAddr = new InetSocketAddress(multicastAddr,port);
+        this.multicastAddr = new InetSocketAddress(multicastAddr, port);
         TIMEOUT = timeout;
         UPDATE_TIMEOUT = updateTimeout;
         socket = new MulticastSocket(port);
         NetworkInterface IFC = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
         socket.setSoTimeout(TIMEOUT);
-        socket.joinGroup(this.multicastAddr,IFC);
+        socket.joinGroup(this.multicastAddr, IFC);
 
 
     }
 
     public void send(String message) throws IOException {
         byte[] messByte = message.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(messByte,messByte.length, multicastAddr);
+        DatagramPacket sendPacket = new DatagramPacket(messByte, messByte.length, multicastAddr);
         socket.send(sendPacket);
         lastSendtime = System.currentTimeMillis();
 
     }
 
-    private String recieve(){
+    private String recieve() {
         byte[] messByte = new byte[128];
-        DatagramPacket recvPacket = new DatagramPacket(messByte,messByte.length);
-        try{
+        DatagramPacket recvPacket = new DatagramPacket(messByte, messByte.length);
+        try {
             socket.receive(recvPacket);
             lastRecvtime = System.currentTimeMillis();
 
         } catch (IOException e) {
-           return null;
+            return null;
         }
         return recvPacket.getAddress().toString();
 
@@ -53,32 +53,29 @@ public class UdpCloneDetector {
         lastSendtime = System.currentTimeMillis();
         Map<String, Long> knownCopies = new HashMap<>();
         LinkedList<String> deffered = new LinkedList<>();
-        while (true){
-            if(System.currentTimeMillis() - lastSendtime > TIMEOUT){
+        while (true) {
+            if (System.currentTimeMillis() - lastSendtime > TIMEOUT) {
                 send("hello");
 
             }
             var localIp = recieve();
-            if(localIp != null) {
+            if (localIp != null) {
                 knownCopies.put(localIp, lastRecvtime);
             }
-            for(Map.Entry<String, Long> entry : knownCopies.entrySet()) {
-                if(System.currentTimeMillis() - entry.getValue() > UPDATE_TIMEOUT){
+            for (Map.Entry<String, Long> entry : knownCopies.entrySet()) {
+                if (System.currentTimeMillis() - entry.getValue() > UPDATE_TIMEOUT) {
                     deffered.add(entry.getKey());
                 }
             }
-            for (var value: deffered) {
+            for (var value : deffered) {
                 knownCopies.remove(value);
 
             }
             deffered.clear();
             System.out.println(knownCopies.size() + " alive!");
-            for(Map.Entry<String, Long> entry : knownCopies.entrySet()) {
-               System.out.println(entry.getKey() + " last seen at " + new Date(entry.getValue()));
+            for (Map.Entry<String, Long> entry : knownCopies.entrySet()) {
+                System.out.println(entry.getKey() + " last seen at " + new Date(entry.getValue()));
             }
-
-
-
 
 
         }
